7ced8cd0d5a6d8f082ad42d40b56c205dd0b4127
Jane Lane <loan.does@gmail.com>
2019-10-01 13:56:46 +0200

    ajout exo9

---
+++ exo_09.rb
@@ -0,0 +1,10 @@
+puts "Bonjour, c'est quoi ton prenom ?"
+user_name = gets.chomp
+puts "Bonjour, c'est quoi ton nom de famille ?"
+user_surname = gets.chomp
+print "Bonjour, "
+print user_name
+print " "
+print user_surname
+print " !"
+
