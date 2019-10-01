05a405a5d56a4dfc7d72104c3cb7d4a5ed84bac8
inesikhlef <ines.ikhlef@outlook.fr>
2019-10-01 11:43:14 +0200

    ajout exo5

---
+++ exo_05.rb
@@ -0,0 +1,23 @@
+#permet d'afficher le résulat sans le calcul et aux collabs de voir les détails du calcul sans l'afficher aux utilisateurs
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}" # 10 heures x 5 jours x 11 semaines = 550h
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #10 heures x 5 jours x 11 semaines x 60 minutes = 33000 minutes
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # va afficher le résultat soit 5
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # va afficher le résultat soit -2
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # affiche la reponse comme 5 est supérieur a -2 le résultat sera true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #affiche la reponse comme 5 est supérieur ou égal a -2 le résultat sera true
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"# affiche la reponse comme 5 est inférieur a -2 le résultat sera false
