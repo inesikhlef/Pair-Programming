e5de4658fdc8699cfc655e236127b577994a6c7a
Jane Lane <loan.does@gmail.com>
2019-10-01 15:48:35 +0200

    maj exo15

--- exo_15.rb
+++ exo_15.rb
@@ -1,9 +1,12 @@
 puts "Quelle est ton annee de naissance ?"
 birth_year = gets.chomp.to_i
 this_year = 2019
+age = #{this_year - birth_year}
 i=0
 while birth_year < this_year
-	puts "#{birth_year+=1}"
-	puts "#{i+=1}"
+	print "En "
+	print "#{birth_year+=1}, "
+	print "tu avais "
+	print "#{age+=1} an.s. "
 end
 
