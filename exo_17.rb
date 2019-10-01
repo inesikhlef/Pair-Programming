fb567d70449a22201e7787c7a80de3395d7a9f88
Jane Lane <loan.does@gmail.com>
2019-10-01 17:30:12 +0200

    maj exo17 ajout exo18

--- exo_17.rb
+++ exo_17.rb
@@ -4,11 +4,12 @@ age = gets.chomp.to_i #age soit x
 this_year = 2019 
 age_times = 0 # il y a tant d'annees soit y
 
-while age >= 0
-	puts "Il y a #{age} ans, tu avais #{age_times} ans"
+while age > 0
 	age -=1
 	age_times +=1
 	if age == age_times
 		puts "Il y a #{age} ans, tu avais la moitie de l'age que tu as aujourd'hui"
+	else
+		puts "Il y a #{age} ans, tu avais #{age_times} ans"
 	end
 end
---
+++ exo_18.rb
@@ -0,0 +1,8 @@
+
+50.times do |k|
+	if k<10
+		puts "jean.dupont.0#{k+=1}@email.fr"
+	else
+		puts "jean.dupont.#{k+=1}@email.fr"
+	end
+end
